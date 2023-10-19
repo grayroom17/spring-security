40aecb3527e1aedb9222e4aac21f4b7533e68f2b
DeevS <deev@advantum.pro>
2023-10-19 21:41:47 +0300

    Authentication Events. AuthenticationFailureBadCredentialsEvent inheritance

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -1,31 +1,31 @@
 package guru.sfg.brewery.security.listeners;
 
-import guru.sfg.brewery.domain.security.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class AuthenticationSuccessListener {
+public class AuthenticationFailureListener {
 
     @EventListener
-    public void listen(AuthenticationSuccessEvent event) {
+    public void listen(AuthenticationFailureBadCredentialsEvent event) {
 
-        log.debug("User Logged In OK");
+        log.debug("Login failure");
 
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken token) {
-            if (token.getPrincipal() instanceof User user) {
-                log.debug("User name logged in: " + user.getUsername());
+            if (token.getPrincipal() instanceof String) {
+                log.debug("Attempted Username: " + token.getPrincipal());
             }
 
             if (token.getDetails() instanceof WebAuthenticationDetails details) {
                 log.debug("Source IP: " + details.getRemoteAddress());
             }
         }
+
     }
 
 }
