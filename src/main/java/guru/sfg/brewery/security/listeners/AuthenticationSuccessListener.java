7db2490793666c190bd58638e7fa02b0266fe75c
DeevS <deev@advantum.pro>
2023-10-19 21:37:22 +0300

    Authentication Events. AuthenticationSuccessEvent inheritance

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
@@ -1,8 +1,11 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -10,8 +13,19 @@ import org.springframework.stereotype.Component;
 public class AuthenticationSuccessListener {
 
     @EventListener
-    public void listen(AuthenticationSuccessEvent event){
-      log.debug("User Logged In OK");
+    public void listen(AuthenticationSuccessEvent event) {
+
+        log.debug("User Logged In OK");
+
+        if (event.getSource() instanceof UsernamePasswordAuthenticationToken token) {
+            if (token.getPrincipal() instanceof User user) {
+                log.debug("User name logged in: " + user.getUsername());
+            }
+
+            if (token.getDetails() instanceof WebAuthenticationDetails details) {
+                log.debug("Source IP: " + details.getRemoteAddress());
+            }
+        }
     }
 
 }
