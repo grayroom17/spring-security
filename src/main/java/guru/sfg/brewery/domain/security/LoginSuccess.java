af8ae16e54b0be84278a5e1f05c761ddf56a3cef
DeevS <deev@advantum.pro>
2023-10-20 22:03:36 +0300

    Authentication Events. Persistence of Authentication Success Events

---
+++ src/main/java/guru/sfg/brewery/domain/security/LoginSuccess.java
@@ -0,0 +1,33 @@
+package guru.sfg.brewery.domain.security;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class LoginSuccess {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    private User user;
+
+    private String sourceIp;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+}
