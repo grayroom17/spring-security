9e1b66aeb8c65e957b9ebdbafa82a3323aa80ae0
DeevS <deev@advantum.pro>
2023-10-27 22:49:34 +0300

    Two-Factor Authentication. Generate QR Code for Google Authenticator

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -1,10 +1,14 @@
 package guru.sfg.brewery.web.controllers;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,12 +24,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
     UserRepository userRepository;
+    GoogleAuthenticator googleAuthenticator;
 
     @GetMapping("/register2fa")
     public String register2fa(Model model) {
-        model.addAttribute("googleUrl", "todo");
+        User user = getUser();
+
+        String url = GoogleAuthenticatorQRGenerator.getOtpAuthURL("SFG",
+                user.getUsername(),
+                googleAuthenticator.createCredentials(user.getUsername()));
+
+        log.debug("Google QR URL: " + url);
+
+        model.addAttribute("googleUrl", url);
 
         return "user/register2fa";
+    }
+
+    private static User getUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
     @PostMapping("/register2fa")
